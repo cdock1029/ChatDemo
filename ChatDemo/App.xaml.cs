0c8029e97a246f48c72c07519f4fde329b940942
Conor Dockry <conordockry@gmail.com>
2025-03-04 14:26:53 -0500

    Add project files.

---
+++ ChatDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using static ChatDemo.Startup;
+
+namespace ChatDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Init();
+        InitializeComponent();
+    }
+}
---
+++ ChatDemo/Startup.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+using ChatDemo.Data;
+using DevExpress.AIIntegration;
+using DevExpress.Blazor;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace ChatDemo;
+
+#pragma warning disable SKEXP0001
+
+public static class Startup
+{
+    public static IServiceProvider? Services { get; private set; }
+
+    public static void Init()
+    {
+        IHost host = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((ctx, builder) => { builder.AddUserSecrets<App>(); })
+            .ConfigureServices(WireupServices)
+            .Build();
+
+        Services = host.Services;
+    }
+
+    private static void WireupServices(HostBuilderContext ctx, IServiceCollection services)
+    {
+        services.AddWpfBlazorWebView();
+        services.AddDevExpressBlazor(configure => configure.BootstrapVersion = BootstrapVersion.v5);
+        services.AddSingleton<WeatherForecastService>();
+
+        ChatConfig? chatConfig = ctx.Configuration.GetSection("ChatConfig").Get<ChatConfig>();
+
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(apiKey: chatConfig!.OpenAiApiKey, modelId: "gpt-4o-mini").Build();
+
+        IChatClient client = kernel.GetRequiredService<IChatCompletionService>().AsChatClient();
+
+        services.AddSingleton(client);
+        services.AddDevExpressAI(settings => settings.RegisterAIExceptionHandler(new ChatExceptionHandler()));
+
+
+#if DEBUG
+        services.AddBlazorWebViewDeveloperTools();
+#endif
+    }
+
+    private sealed class ChatConfig
+    {
+        public string OpenAiApiKey { get; init; } = null!;
+    }
+
+    private class ChatExceptionHandler : IAIExceptionHandler
+    {
+        public Exception ProcessException(Exception e)
+        {
+            Debug.WriteLine($"AI Exception in ChatDemo: {e.Message}");
+            return e;
+        }
+    }
+}
